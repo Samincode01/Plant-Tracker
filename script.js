79e6a87955a3d3c07fab39dc6cab31f9733ea29e
Samincode01 <samin990bd@gmail.com>
2026-03-07 06:08:44 +0600

    Finished project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let thrivingList= [];
 let strugglingList= [];
+let currentStatus = 'all'
 
 let total= document.getElementById("total");
 let strugglingCount= document.getElementById("strugglingCount");
@@ -11,16 +12,16 @@ const strugglingfilterBtn = document.getElementById('struggling-filter-btn')
 
 const allCardSection= document.getElementById("allCard");
 const mainContainer= document.querySelector("main")
-const filterSection= document.getElementsByClass("filtered-Section")
+const filterSection= document.getElementById("filtered-Section")
 
-function totalCount()
+function caclculateCount()
 {
     total.innerText= allCardSection.children.length;
     thriveCount.innerText= thrivingList.length;
     strugglingCount.innerText= strugglingList.length;
 }
 
-totalCount();
+caclculateCount();
 
 function toggleStyle(id)
 {
@@ -35,10 +36,30 @@ function toggleStyle(id)
     const selected = document.getElementById(id);
     selected.classList.remove('bg-gray-300','text-black')
     selected.classList.add('bg-black', 'text-white')
+
+    if(id == 'thriving-filter-btn')
+    {
+        allCardSection.classList.add('hidden')
+        filterSection.classList.remove('hidden')
+    }
+    else if(id == 'all-filter-btn')
+    {
+        allCardSection.classList.remove('hidden')
+        filterSection.classList.add('hidden')
+    }
+    else if(id == 'struggling-filter-btn')
+    {
+        allCardSection.classList.remove('hidden')
+        filterSection.classList.add('hidden')
+        renderStruggling()
+    }
+
 }
 
 mainContainer.addEventListener('click',function(event){
-    const parenNode= event.target.parentNode.parentNode
+    if(event.target.classList.contains('thrive-btn'))
+    {
+        const parenNode= event.target.parentNode.parentNode
     const plantName = parenNode.querySelector('.plantName').innerText
     const water = parenNode.querySelector('.water').innerText
     const light = parenNode.querySelector('.light').innerText
@@ -56,16 +77,47 @@ mainContainer.addEventListener('click',function(event){
     {
         thrivingList.push(cardInfo)
     }
-    console.log(thrivingList)
+    strugglingList = strugglingList.filter(item=>item.plantName !=cardInfo.plantName)
+     if (currentStatus == 'struggling-filter-btn') {
+            renderStruggling()
+        }
+    caclculateCount()
+    
+}    
+else if(event.target.classList.contains('struggling-btn'))
+    {
+        const parenNode= event.target.parentNode.parentNode
+    const plantName = parenNode.querySelector('.plantName').innerText
+    const water = parenNode.querySelector('.water').innerText
+    const light = parenNode.querySelector('.light').innerText
+    const notes = parenNode.querySelector('.notes').innerText
+
+    const cardInfo = {
+        plantName,
+        light,
+        water,
+        notes
+    }
+    const plantExist = strugglingList.find(item => item.plantName == cardInfo.plantName)
+
+    if(!plantExist)
+    {
+        strugglingList.push(cardInfo)
+    }
+    
+    thrivingList = thrivingList.filter(item=>item.plantName !=cardInfo.plantName)
+    caclculateCount()
+    renderStruggling()
+}
 
 })
-
-function renderThriving()
+    function renderThriving()
 {
-    filterSection.innetHTML =''
+    filterSection.innerHTML =''
     
 
     for(let thrive of thrivingList){
+        console.log(thrive)
      div = document.createElement('div');
         div.className = 'card flex justify-between border p-8'
     div.innerHTML = `<div class="space-y-6">
@@ -88,6 +140,40 @@ function renderThriving()
                 <div>
                     <button class="btn-delete bg-red-200 text-red-600 px-4 py-2">Delete</button>
                 </div>`
+                filterSection.appendChild(div)
+                caclculateCount()
+    }
     }
 
-}
+    function renderStruggling()
+{
+    filterSection.innerHTML =''
+    
+
+    for(let Struggle of strugglingList){
+     div = document.createElement('div');
+        div.className = 'card flex justify-between border p-8'
+    div.innerHTML = `<div class="space-y-6">
+                    <div>
+                        <p class="plantName text-4xl">Plant name 2</p>
+                        <p class="latinName">Latin name</p>
+                    </div>
+
+                    <div class="flex gap-2">
+                        <p class="light bg-gray-200 px-5" >Bright Indicate</p>
+                        <p class="water bg-gray-200 px-5" >Weekly</p>
+                    </div>
+                    <p class="status">not applicable</p>
+                    <p class="notes">Lorem ipsum dolor sit amet.</p>
+                    <div class="flex gap-5">
+                        <button class="thrive-btn bg-green-200 px-4 py-2">Thrive</button>
+                        <button class="struggling-btn bg-red-200 px-4 py-2">Struggle</button>
+                    </div>
+                </div>
+                <div>
+                    <button class="btn-delete bg-red-200 text-red-600 px-4 py-2">Delete</button>
+                </div>`
+                filterSection.appendChild(div)
+                caclculateCount()
+    }
+    }
